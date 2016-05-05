c6490a6793332a5107daef4d6319336b2968d896
Jesse Sopel <jesse.sopel@libeo.com>
2016-05-05 13:40:39 -0400

    [FEATURE] Also include streamfields in possible translated fields

--- wagtail_modeltranslation/static/modeltranslation/js/wagtail_language_picker.js
+++ wagtail_modeltranslation/static/modeltranslation/js/wagtail_language_picker.js
@@ -2,6 +2,15 @@ $(function() {
     var $fields = $('ul.objects');
     var $languagePickerDiv = $('.language-picker');
     if ($languagePickerDiv.length > 0 && $fields.length > 0) {
+        $fields.find('.stream-field').each(function (i, el) {
+            // Fin the language code, if applicable, and add the appropriate classes to the stream field
+            var matches = $(el).find('legend').text().match(/\[([a-z]+)\]/);
+            if (matches.length > 1 && langs.indexOf(matches[1]) !== -1) {
+                $(el).addClass('translation-field lang-' + matches[1]);
+            }
+        });
+
+
         var $translationFields = $fields.find('li.translation-field');
 
         var $headerColumn = $('header .row .col3');
