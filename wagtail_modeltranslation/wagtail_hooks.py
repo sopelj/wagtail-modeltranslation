5bb1a7073d1ec523778eac42bb040a275f6a9c0a
Jesse Sopel <jesse.sopel@libeo.com>
2016-05-05 10:46:31 -0400

    [FIX] Fixed typo and import only js on editor page

--- wagtail_modeltranslation/wagtail_hooks.py
+++ wagtail_modeltranslation/wagtail_hooks.py
@@ -109,7 +109,8 @@ def streamfields_translation_copy():
 
     return js_includes
 
-@hooks.register('insert_global_admin_js')
+
+@hooks.register('insert_editor_js')
 def add_language_picker_js():
     """
     Includes script in editor html file that controls the
@@ -119,12 +120,12 @@ def add_language_picker_js():
 
     # includes the javascript file in the html file
     js_files = [
-        'modeltranslation/js/wagatail_language_picker.js',
+        'modeltranslation/js/wagtail_language_picker.js',
     ]
 
     js_includes = format_html_join('\n', '<script src="{0}{1}"></script>', (
         (settings.STATIC_URL, filename) for filename in js_files)
-                                   )
+    )
 
     return js_includes
 
